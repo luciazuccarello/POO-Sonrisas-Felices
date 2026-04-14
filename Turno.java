9d3313e8d3652594d707b0e47f9eb673be83f40a
luciazuccarello <luciazuccarellogit@gmail.com>
2026-04-14 09:51:24 -0300

    Refactor Turno class to include EstadoTurno and auto-increment ID

--- Turno.java
+++ Turno.java
@@ -3,52 +3,65 @@ package model;
 import java.util.Date;
 
 public class Turno {
-
+    private static int contador = 1;
     private Integer id;
     private Paciente paciente;
     private Odontologo odontologo;
     private Date fecha;
     private Date hora;
-    private boolean activo;
+    private EstadoTurno estado;
 
-    public Turno() {}
+    public Turno() {
+        this.id = contador++;
+    }
 
-    public Turno(Integer id, Paciente paciente, Odontologo odontologo, Date fecha, Date hora, boolean activo) {
+    public Turno(Integer id, Paciente paciente, Odontologo odontologo, Date fecha, Date hora, EstadoTurno estado) {
         this.id = id;
         this.paciente = paciente;
         this.odontologo = odontologo;
         this.fecha = fecha;
         this.hora = hora;
-        this.activo = activo;
+        this.estado = estado;
     }
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+    public Integer getId() {
+        return id; }
+    public void setId(Integer id) {
+        this.id = id; }
 
-    public Paciente getPaciente() { return paciente; }
-    public void setPaciente(Paciente paciente) { this.paciente = paciente; }
+    public Paciente getPaciente() {
+        return paciente; }
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente; }
 
-    public Odontologo getOdontologo() { return odontologo; }
-    public void setOdontologo(Odontologo odontologo) { this.odontologo = odontologo; }
+    public Odontologo getOdontologo() {
+        return odontologo; }
+    public void setOdontologo(Odontologo odontologo) {
+        this.odontologo = odontologo; }
 
-    public Date getFecha() { return fecha; }
-    public void setFecha(Date fecha) { this.fecha = fecha; }
+    public Date getFecha() {
+        return fecha; }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha; }
 
-    public Date getHora() { return hora; }
-    public void setHora(Date hora) { this.hora = hora; }
+    public Date getHora() {
+        return hora; }
+    public void setHora(Date hora) {
+        this.hora = hora; }
 
-    public boolean isActivo() { return activo; }
-    public void setActivo(boolean activo) { this.activo = activo; }
+    public EstadoTurno getEstado() {
+        return estado;
+    }
+    public void setEstado(EstadoTurno estado) {
+        this.estado = estado;
+    }
 
     @Override
     public String toString() {
-        return "Turno{" +
-                "id=" + id +
-                ", paciente=" + paciente +
-                ", odontologo=" + odontologo +
-                ", fecha=" + fecha +
-                ", hora=" + hora +
-                ", activo=" + activo +
-                '}';
+        return "ID turno: " + id + "\n" +
+                "Paciente: " + paciente.getNombre() + " " + paciente.getApellido() + " (ID Paciente: " + paciente.getId() + " )\n" +
+                "Odontólogo: " + odontologo.getNombre() + " " + odontologo.getApellido() + " (ID Odontólogo: " + odontologo.getId() + ")\n" +
+                "Fecha: " + fecha + "\n" +
+                "Estado: " + estado;
     }
 }
