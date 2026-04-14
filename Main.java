2fccbf1392d99fd7f5eecd27d1f544845559dc9d
luciazuccarello <luciazuccarellogit@gmail.com>
2026-04-14 09:48:31 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,25 @@
-package main;
-
-import model.*;
-
+package model;
 import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
 
         //  Crear domicilio
-        Domicilio domicilio = new Domicilio( "Av. Siempre Viva", 742, "Springfield", "Buenos Aires");
+        Domicilio domicilio = new Domicilio( "Av. Avellaneda", 742, "Caballito", "Buenos Aires");
 
         //  Crear paciente
-        Paciente paciente = new Paciente(1, "Juan", "Perez", "12345678", new Date(), domicilio);
+        Paciente paciente = new Paciente(1, "Juan", "Perez", "12345678", "juanperez@gmail.com" ,new Date(), domicilio);
 
         // ️ Crear odontólogo
-        Odontologo odontologo = new Odontologo(1, "Ana", "Gomez", "MAT-123");
+        Odontologo odontologo = new Odontologo(1, "Ana", "Gomez", "anagomez@gmail.com", "MAT-123");
 
         //  Crear recepcionista
-        Recepcion recepcionista = new Recepcion("Laura");
+        Recepcion recepcionista = new Recepcion("Laura", "Martinez", "sonrisasfelices@gmail.com");
 
         //  Crear turno usando la recepcionista
-        Turno turno = recepcionista.crearTurno(paciente, odontologo, new Date(), new Date());
+        Turno t1 = recepcionista.crearTurno(paciente, odontologo, new Date(), new Date(), EstadoTurno.CONFIRMADO);
 
-        //  Imprimir todo
-        System.out.println("----- DOMICILIO -----");
+        //  Imprimir
+        System.out.println("\n----- DOMICILIO -----");
         System.out.println(domicilio);
 
         System.out.println("\n----- PACIENTE -----");
@@ -33,19 +29,27 @@ public class Main {
         System.out.println(odontologo);
 
         System.out.println("\n----- TURNO -----");
-        System.out.println(turno);
+        System.out.println(t1);
 
         System.out.println("\n----- RECEPCIONISTA -----");
         System.out.println(recepcionista);
 
         //  Cancelar turno
-        recepcionista.cancelarTurno(turno);
+        recepcionista.cancelarTurno(t1);
         System.out.println("\n----- TURNO CANCELADO -----");
-        System.out.println(turno);
+        System.out.println(t1);
 
         //  Reprogramar turno
-        recepcionista.reprogramarTurno(turno, new Date(), new Date());
-        System.out.println("\n----- TURNO REPROGRAMADO -----");
-        System.out.println(turno);
+        //Guardo fecha anterior
+        Date fechaAnterior = t1.getFecha();
+        //Crear nueva fecha
+        Date nuevaFecha = new Date(System.currentTimeMillis() + 86400000); // +1 dìa
+        Date nuevaHora = nuevaFecha;
+        recepcionista.reprogramarTurno(t1, nuevaFecha, nuevaHora);
+        System.out.println ("\n----- TURNO REPROGRAMADO -----");
+        System.out.println("ID turno: " + t1.getId());
+        System.out.println("Turno anterior: " + fechaAnterior);
+        System.out.println("Nuevo turno: " + t1.getFecha());
+        System.out.println("Estado: " + t1.getEstado());
     }
 }
