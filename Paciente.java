aa6bd6b348a369809715a4825b736f9c7e9f62f2
luciazuccarello <luciazuccarellogit@gmail.com>
2026-04-14 09:50:23 -0300

    Add mail attribute to Paciente class
    Added 'mail' attribute and updated constructors, getters, and toString method.

--- Paciente.java
+++ Paciente.java
@@ -8,47 +8,86 @@ public class Paciente {
     private String nombre;
     private String apellido;
     private String dni;
+    private String mail;
     private Date fechaAlta;
     private Domicilio domicilio;
 
-    public Paciente() {}
+    public Paciente() {
+    }
 
-    public Paciente(Integer id, String nombre, String apellido, String dni, Date fechaAlta, Domicilio domicilio) {
+    public Paciente(Integer id, String nombre, String apellido, String dni, String mail, Date fechaAlta, Domicilio domicilio) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
+        this.mail = mail;
         this.fechaAlta = fechaAlta;
         this.domicilio = domicilio;
     }
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+    public Integer getId() {
+        return id;
+    }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public String getApellido() { return apellido; }
-    public void setApellido(String apellido) { this.apellido = apellido; }
+    public String getNombre() {
+        return nombre;
+    }
 
-    public String getDni() { return dni; }
-    public void setDni(String dni) { this.dni = dni; }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-    public Date getFechaAlta() { return fechaAlta; }
-    public void setFechaAlta(Date fechaAlta) { this.fechaAlta = fechaAlta; }
+    public String getApellido() {
+        return apellido;
+    }
 
-    public Domicilio getDomicilio() { return domicilio; }
-    public void setDomicilio(Domicilio domicilio) { this.domicilio = domicilio; }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getMail (String mail) {
+        return mail;
+    }
+
+    public void setMail (String mail) {
+        this.mail = mail;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
 
     @Override
     public String toString() {
-        return "Paciente{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", dni='" + dni + '\'' +
-                ", fechaAlta=" + fechaAlta +
-                ", domicilio=" + domicilio +
-                '}';
+        return "ID Paciente: " + id + "\n" +
+                "Nombre: " + nombre + " " + apellido + "\n" +
+                "DNI: " + dni + "\n" +
+                "Mail: " + mail + "\n" +
+                "Fecha de alta: " + fechaAlta + "\n" +
+                "Domicilio: " + domicilio;
     }
 }
